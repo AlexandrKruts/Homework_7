2d6a9270a32ee6085d3e6ed4ffd67663eb27530c
AlexandrKruts <kruts704@gmail.com>
2022-08-21 15:09:52 +0300

    Done second task

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@
 
 // console.log(sumTo(4)); // 10
 // console.log(sumTo(3)); // 6
+
+//2
+
+// function findShort(str) {
+//   str = str.split(" ");
+//   str.sort((a, b) => a.length - b.length);
+//   return str[0];
+// }
+
+// const sentance = "Lorem ipsum dolor sit amet";
+
+// console.log(findShort(sentance)); // sit
+// console.log(findShort("Hello world")); // Hello
+// console.log(findShort("Hi")); // Hi
+// console.log(findShort("She is David's sister")); // is
