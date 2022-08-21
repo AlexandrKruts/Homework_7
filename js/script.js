d3b5cde3aa6635dec31426bd67dcac4b2361b69e
AlexandrKruts <kruts704@gmail.com>
2022-08-21 15:09:23 +0300

    Done first task

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//1
+
+// function sumTo(n) {
+//   if (n == 1){
+//     return 1;
+//   }
+
+//   return n + sumTo(n - 1);
+// }
+
+// console.log(sumTo(4)); // 10
+// console.log(sumTo(3)); // 6
